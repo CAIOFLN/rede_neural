b4b29d56c080d9ed1381c00792e6c6675d8bb6d3
CAIOFLN <caio.florentin@usp.br>
2024-05-30 14:08:23 -0300

    novo

--- verifica_base.py
+++ verifica_base.py
@@ -1,41 +1,41 @@
-import torch
-from torchvision import transforms
-from PIL import Image
+import json
+from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
-# Função para carregar e visualizar o dataset salvo
-def visualize_saved_dataset(file_path, num_samples = 10):
-    # Carregar o dataset salvo
-    data_list = torch.load(file_path)
-    
-    # Verificar algumas amostras do dataset
-    for i in range(min(num_samples, len(data_list))):
-        img, boxes, labels, img_path = data_list[i]
+# Carregar dados do JSON
+with open('simple_coco_ann_train.json', 'r') as f:
+    data = json.load(f)
+# Função para desenhar bounding boxes nas imagens
 
-        # Converter a imagem de tensor para PIL Image para visualização
-        img = transforms.ToPILImage()(img)
-        
-        # Plotar a imagem
-        fig, ax = plt.subplots(1)
-        ax.imshow(img)
-        
-        # Adicionar as bounding boxes
-        for box in boxes:
-            # COCO bounding boxes estão no formato [x, y, largura, altura]
-            x, y, w, h = box
-            rect = patches.Rectangle((x, y), w, h, linewidth=2, edgecolor='r', facecolor='none')
-            ax.add_patch(rect)
-        
-        plt.title(f'Sample {i+1}: {img_path}')
-        plt.show()
+def draw_bboxes(image_info, annotations):
+    img_path = 'base_de_dados/train/' + image_info['file_name']
+    img = Image.open(img_path).convert("RGB")
+    draw = ImageDraw.Draw(img)
 
-# Caminho para o dataset salvo
-file_path_train = 'coco_train_dataset.pt'
-file_path_test = 'coco_test_dataset.pt'
+    for ann in annotations:
+        if ann['image_id'] == image_info['img_id']:
+            bbox = ann['bbox']
+            x, y, w, h = bbox
+            draw.rectangle([x, y, x+w, y+h], outline="red", width=2)
 
-# Visualizar algumas amostras do dataset de treino
-visualize_saved_dataset(file_path_train)
+    return img
 
-# Visualizar algumas amostras do dataset de teste
-visualize_saved_dataset(file_path_test)
+# Mostrar imagens com bounding boxes
+def show_images_with_bboxes(data, num_images=5):
+    images = data['images']
+    annotations = data['annotations']
+    selected_images = images[:num_images]
+
+    plt.figure(figsize=(15, 15))
+
+    for i, image_info in enumerate(selected_images):
+        img_with_bboxes = draw_bboxes(image_info, annotations)
+        plt.subplot(1, num_images, i + 1)
+        plt.imshow(img_with_bboxes)
+        plt.axis('off')
+        plt.title(f"Image ID: {image_info['img_id']}")
+
+    plt.show()
+
+# Executar a função para mostrar imagens com bounding boxes
+show_images_with_bboxes(data, num_images=10)
